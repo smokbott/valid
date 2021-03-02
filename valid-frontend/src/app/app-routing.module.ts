b357798f18855a34a4ad29df5f839e87ad85b836
dibanez <ingeniero.ibanez.sys@gmail.com>
2021-03-02 13:38:58 -0500

    fix url

--- valid-frontend/src/app/app-routing.module.ts
+++ valid-frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonComponent } from './components/person/person.component';
 
-const routes: Routes = [{ path: "", redirectTo: "/persons", pathMatch: "full" },
+const routes: Routes = [{ path: "", redirectTo: "/valid/persons", pathMatch: "full" },
 { path: "persons", component: PersonComponent }];
 
 @NgModule({
